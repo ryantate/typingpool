70e15abb4ee09758e5512eb7835a5b071f9fa41c
Ryan Tate <ryantate@ryantate.com>
2011-08-03 18:22:28 -0700

    Broke out CSS into its own template; renamed HTML template to 'transcript' instead of 'transcription'

--- bin/collect.rb
+++ bin/collect.rb
@@ -5,18 +5,22 @@ require 'audibleturk'
 
 #Name of form field in Mechanical Turk assignment form
 # containing URL to audio file
-url_at = ARGV[0] || 'audibleturk_url'
+url_at_form_field = ARGV[0] || 'audibleturk_url'
 
 home = "#{Dir.home}/Documents/Software/dist/ruby/audibleturk"
 
 puts "Collecting results from Amazon..."
-results = Audibleturk::Remote::Result.all_approved(:url_at => url_at)
+results = Audibleturk::Remote::Result.all_approved(:url_at => url_at_form_field)
 
 puts "Sorting results..."
 available_folders = results.collect{|result| result.transcription.title }.uniq.select{|title| Audibleturk::Folder.named(title)}
 
-template = IO.read("#{home}/www/transcription.html.erb") unless available_folders.empty?
-
+unless available_folders.empty?
+  template = {
+    'html' => IO.read("#{home}/www/transcript.html.erb"),
+    'css' => IO.read("#{home}/www/transcript.css.erb")
+  }
+end
 available_folders.each do |folder|
   shortname = folder
   folder = Audibleturk::Folder.named(folder)
@@ -25,19 +29,19 @@ available_folders.each do |folder|
     'working' => 'transcript_in_progress.html'
   }
   next if File.exists?("#{folder.path}/#{filename['done']}")
+  css = ERB.new(template['css'], nil, '<>').result(binding())
+  File.open("#{folder.path}/etc/transcript.css", 'w') do |out|
+    out << css
+  end
   transcription = Audibleturk::Transcription.new(shortname, results.select{|result| result.transcription.title == shortname}.collect{|result| result.transcription})
   transcription.each{|chunk| chunk.url = "audio/#{chunk.filename_local}" }
   transcription.subtitle = folder.subtitle
-  html = ERB.new(template, nil, '<>').result(binding())
   File.delete("#{folder.path}/#{filename['working']}") if File.exists?("#{folder.path}/#{filename['working']}")
   is_done = (transcription.to_a.length == folder.audio_chunks)
   out_file = is_done ? filename['done'] : filename['working']
-  out_paths = ["#{folder.path}/#{out_file}"]
-  out_paths.push("#{folder.path}/originals/#{out_file}") if is_done
-  out_paths.each do |path|
-    File.open(path, 'w') do |out|
-      out << html
-    end
+  html = ERB.new(template['html'], nil, '<>').result(binding())
+  File.open("#{folder.path}/#{out_file}", 'w') do |out|
+    out << html
   end
   puts "Wrote #{out_file} to folder #{shortname}."
 end
