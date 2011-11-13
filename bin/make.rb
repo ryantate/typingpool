5a3280dcdf0609858f729fd4874a8d9ed65dff26
Ryan Tate <ryantate@ryantate.com>
2011-11-13 14:44:48 -0800

    Improvements to make.rb USAGE and --help screens

--- bin/make.rb
+++ bin/make.rb
@@ -10,51 +10,51 @@ options = {
   :chunk => '1:00'
 }
 OptionParser.new do |opts|
-  options[:banner] = opts.banner = "USAGE: tp-make --file foo.mp3 [--file bar.mp3...] --title Foo --chunks 1:00 [--subtitle 'Foo telephone interview about Yahoo Hack Day'][--voice 'John Foo' --voice 'Sally Bar, Female interiewer with British accent'][--unusual 'Yahoo' --unusual 'Hack Day' --unusual 'Sunnyvale, Chad Dickerson, Zawodny'][--moveorig]\n"
+  options[:banner] = opts.banner = "USAGE: #{File.basename($PROGRAM_NAME)} --title Foo --file foo.mp3 [--file bar.mp3...] [--chunks 1:00] [--subtitle 'Interview about Hack Day'][--voice 'John Foo' --voice 'Sally Bar, female voice'][--unusual 'Hack Day' --unusual 'Hack Day, Sunnyvale, Dickerson'][--moveorig]\n--help for more\n"
 
-  opts.on('--file FILE', 'Audio file to be transcribed. Required. May be specified repeatedly for multiple files. FILES WILL BE SORTED naively, so order is unimportant.') do |file|
+  opts.on('--file FILE', 'Required. Audio file to be transcribed. Specify repeatedly for multiple files (will be sorted by name).') do |file|
     options[:files].push(file)
   end
 
-  opts.on('--title TITLE', 'Title for naming files and the transcript. Required.') do |title|
+  opts.on('--title TITLE', 'Required. Title for naming files and the transcript.') do |title|
     options[:title] = title
   end
 
-  opts.on('--subtitle SUBTITLE', 'Subtitle for the transcript. Optional.') do |subtitle|
+  opts.on('--subtitle SUBTITLE', 'Subtitle for the transcript.') do |subtitle|
     options[:subtitle] = subtitle
   end
 
-  opts.on('--chunks MM:SS' 'Audio will be divided into chunks for transcribing, each this many minutes and seconds long. Optional. Default is 1:00.  Format can be MM:SS, SS, or HH:MM:SS, plus optional decimal values (MM:SS.ss etc.)') do |chunk|
+  opts.on('--chunks MM:SS' 'Default: 1:00. Audio divded into chunks of this length for transcribing. Format expandable to HH:MM:SS.ss') do |chunk|
     options[:chunk] = chunk
   end
 
-  opts.on('--voice "NAME[, TITLE]"', 'Name and optional title of a person in the transcript. Optional. May be specified repeatedly for multiple voices.') do |voice|
+  opts.on('--voice "NAME[, TITLE]"', 'Name and optional title of a person in the transcript. Specify repeatedly for multiple voices.') do |voice|
     options[:voices].push(voice)
   end
 
-  opts.on('--unusual WORD[,WORD, WORD,...]', 'An unusual word occuring in the transcript, to help the transcriber. Optional. May be specified repeatedly for multiple words. May accept multiple arguments with commas.') do |word|
+  opts.on('--unusual WORD[,WORD, WORD,...]', 'Unusual word occuring in the transcript, to help the transcriber. Specify repeatedly for multiple words, or separate with commas.') do |word|
     options[:unusual].push(word)
   end
 
-  opts.on('--config PATH', 'Alternate config file to use. Optional. Default is ~/.audibleturk') do |config|
+  opts.on('--config PATH', 'Default: ~/.audibleturk. Path to a config file.') do |config|
     options[:config] = config
   end
 
-  opts.on('--bitrate KBPS', 'Bitrate for output files, expressed as an integer representing kilobits per second. Optional. Default is to match the bitrate of the input files.') do |kbps|
+  opts.on('--bitrate KBPS', 'Default: Input file bitrate. Output file bitrate, as an integer representing kilobits per second.') do |kbps|
     options[:bitrate] = kbps
   end
 
-  opts.on('--moveorig', 'Move the original files into instead of copying them.') do
+  opts.on('--moveorig', 'Move audio input files instead of copying.') do
     options[:moveorig] = true
   end
 
-  opts.on('--help', 'Display this screen') do
+  opts.on('--help', 'Display this screen.') do
     puts opts
     exit
   end
 end.parse!
 
-abort "No files specified" if options[:files].empty?
+abort "No files specified.\n#{options[:banner]}" if options[:files].empty?
 options[:files].sort!
 options[:files].each do |file|
   File.extname(file) or abort "You need a file extension on the file '#{file}'"
