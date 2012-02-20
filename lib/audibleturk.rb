9849cad2fef18c894d7046280c4e3d76eebd2400
Ryan Tate <ryantate@ryantate.com>
2012-02-20 15:21:17 -0800

    Utility.system_quietly returns STDOUT on success, when available

--- lib/audibleturk.rb
+++ lib/audibleturk.rb
@@ -20,14 +20,19 @@ module Audibleturk
     def self.system_quietly(*cmd)
       exit_status=nil
       err=nil
+      out=nil
       Open3.popen3(*cmd) do |stdin, stdout, stderr, wait_thread|
         yield(stdin, stdout, stderr, wait_thread) if block_given?
         err = stderr.gets(nil)
+        out = stdout.gets(nil)
         [stdin, stdout, stderr].each{|stream| stream.send('close')}
         exit_status = wait_thread.value
       end
       if exit_status.to_i > 0
+        err = err.chomp if err
         raise Audibleturk::Error::Shell, err
+      elsif out
+        return out.chomp
       else
         return true
       end
