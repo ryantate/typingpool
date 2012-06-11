4ba87c3f0277ad69a7775481bed978ca5ddc330e
Ryan Tate <ryantate@ryantate.com>
2012-06-10 17:39:43 -0700

    Adds unit tests for Transcript, related fixture, and related fixture maker.

---
+++ test/make_transcript_chunks_fixture.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+$:.unshift File.join(File.dirname(File.dirname($0)), 'lib')
+
+require 'typingpool'
+
+lines_each = 2
+if ARGV.last.to_s.match(/^\d+$/)
+  lines_each = ARGV.pop.to_i
+end
+
+if ARGV.empty?
+  abort "USAGE: #{File.basename($PROGRAM_NAME)} PROJECT_DIR [PROJECT_DIR...] [LINES_EACH=2]"
+end
+
+data_files = ARGV.map do |path| 
+  data_file = nil
+  expanded = File.expand_path(path)
+  if not(File.exists? expanded)
+    abort "No such dir #{path}"
+  end
+  if not(File.directory? expanded)
+    abort "Not a dir: #{path}"
+  end
+  %w(data csv).each do |data_dir_name|
+    possible_data_file = File.join(expanded, data_dir_name, 'assignment.csv')
+    if File.exists? possible_data_file
+      data_file = possible_data_file
+    end
+  end #%w().each do...
+  if not(data_file)
+    abort "Dir #{path} has no data/assignment.csv or csv/assignment.csv"
+  end
+  data_file
+end #ARGV.map
+
+assignments=[]
+data_files.each do |path|
+  csv = Typingpool::Filer::CSV.new(path)
+  with_transcripts = csv.reject{|assignment| assignment['transcription'].to_s.empty? }
+  next if with_transcripts.empty?
+  seeking = with_transcripts.count
+  if seeking > lines_each
+    seeking = lines_each
+  end
+  assignments.push(*with_transcripts.sample(seeking))
+end #data_files.each do...
+
+if assignments.empty?
+  abort "No transcripts found"
+end
+fixtures_dir = File.join(File.dirname(File.dirname($0)), 'test', 'fixtures')
+Typingpool::Filer::CSV.new(File.join(fixtures_dir, 'transcript-chunks.csv')).write(assignments)
---
+++ test/test_unit_transcript.rb
@@ -0,0 +1,77 @@
+#!/usr/bin/env ruby
+
+$:.unshift File.join(File.dirname(File.dirname($0)), 'lib')
+
+require 'typingpool'
+require 'typingpool/test'
+
+class TestTranscript < Typingpool::Test
+
+  def test_transcript_base
+    assert(transcript = Typingpool::Transcript.new(project_default[:title]))
+    assert_equal(project_default[:title], transcript.title)
+    assert_equal(0, transcript.count)
+    dummy_chunks = self.dummy_chunks
+    assert(transcript = Typingpool::Transcript.new(project_default[:title], dummy_chunks))
+    assert_equal(dummy_chunks.count, transcript.count)
+    assert_respond_to(transcript, :each)
+    transcript.each_with_index do |chunk, i| 
+      assert_respond_to(chunk, :body)
+      assert_equal(dummy_chunks[i].body, chunk.body)
+      assert_equal(chunk.body, transcript[i].body)
+    end #transcript.each_with_index do...
+    assert(transcript = Typingpool::Transcript.new(project_default[:title]))
+    assert_equal(0, transcript.count)
+    assert_respond_to(transcript, :add_chunk)
+    dummy_chunks.each{|chunk| transcript.add_chunk(chunk) }
+    assert_equal(dummy_chunks.count, transcript.count)
+    random_index = rand(dummy_chunks.count)
+    assert_equal(dummy_chunks[random_index].body, transcript[random_index].body)
+  end
+
+  def test_transcript_chunks
+    dummy_assignments.each do |assignment|
+      assert(chunk = Typingpool::Transcript::Chunk.new(assignment['transcription']))
+      assert_equal(assignment['transcription'], chunk.body)
+      chunk.worker = assignment['worker']
+      assert_equal(assignment['worker'], chunk.worker)
+      chunk.project = assignment['project_id']
+      assert_equal(assignment['project_id'], chunk.project)
+      chunk.hit = assignment['hit_id']
+      assert_equal(assignment['hit_id'], chunk.hit)
+      url_populated = %w(offset offset_seconds filename filename_local)
+      url_populated.each{|attr| assert_nil(chunk.send(attr.to_sym)) }
+      chunk.url = assignment['audio_url']
+      assert_equal(assignment['audio_url'], chunk.url)
+      url_populated.each{|attr| refute_nil(chunk.send(attr.to_sym)) }
+      matches = chunk.offset.match(/^(\d+):(\d+)$/)
+      assert_equal(((matches[1].to_i * 60) + matches[2].to_i), chunk.offset_seconds)
+      refute_equal(chunk.filename, chunk.filename_local)
+      original_newline_count = chunk.body_as_text.scan(/\n/).count
+      chunk.body = chunk.body + "\n\r\f" + ('foo bar baz' * 100) 
+      refute_match(chunk.body_as_text, /\r/) unless $/.match(/\r/)
+      assert_equal(original_newline_count + 2, chunk.body_as_text.scan(/\n/).count)
+      original_p_count = chunk.body_as_html.scan(/<p>/i).count
+      chunk.body = chunk.body + "One & two\n\n...and 3 < 4."
+      refute_match(chunk.body_as_html, /\s&\s/)
+      refute_match(chunk.body_as_html, /\s<\s/)
+      assert_equal(original_p_count + 1, chunk.body_as_html.scan(/<p>/i).count)
+    end
+  end
+
+  def dummy_chunks
+    dummy_assignments.map do |assignment|
+      chunk = Typingpool::Transcript::Chunk.new(assignment['transcription'])
+      chunk.worker = assignment['worker']
+      chunk.project = assignment['project_id']
+      chunk.hit = assignment['hit_id']
+      chunk.url = assignment['audio_url']
+      chunk
+    end #dummy_assignments.map do...
+  end
+
+  def dummy_assignments
+    Typingpool::Filer::CSV.new(File.join(fixtures_dir, 'transcript-chunks.csv')).read
+  end
+
+end #TestTranscript
