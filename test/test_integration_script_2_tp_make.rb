33ba672fe1aa0af8dc473dd6f36d31044b27b9d6
Ryan Tate <ryantate@ryantate.com>
2012-09-03 16:16:25 -0700

    Refactors and adds upload status asserts to tp-make integration test.

--- test/test_integration_script_2_tp_make.rb
+++ test/test_integration_script_2_tp_make.rb
@@ -36,21 +36,16 @@ class TestTpMake < Typingpool::Test::Script
   end
 
   def tp_make_with(dir, config_path, subdir='mp3')
-    project = nil
-    assert_nothing_raised do
+    begin
       tp_make(dir, config_path, subdir)
-      assert_nothing_raised do
-        project = temp_tp_dir_project(dir, Typingpool::Config.file(config_path))
-      end
+      assert(project = temp_tp_dir_project(dir, Typingpool::Config.file(config_path)))
       assert_not_nil(project.local)
       assert_not_nil(project.local.id)
       assert_equal(7, project.local.subdir('audio','chunks').to_a.size)
       assert_equal(project_default[:subtitle], project.local.subtitle)
-      assignments = nil
-      assert_nothing_raised do 
-        assignments = project.local.file('data', 'assignment.csv').as(:csv).read
-      end
-      assert_equal(project.local.subdir('audio','chunks').to_a.size, assignments.size)
+      assignments = project.local.file('data', 'assignment.csv').as(:csv)
+      assert_equal(project.local.subdir('audio','chunks').to_a.size, assignments.count)
+      assert_all_assets_have_upload_status(assignments, ['audio'], 'yes')
       assignments.each do |assignment|
         assert_not_nil(assignment['audio_url'])
         assert(working_url? assignment['audio_url'])
@@ -62,10 +57,13 @@ class TestTpMake < Typingpool::Test::Script
           if not(description.to_s.empty?)
             assert_equal(description, assignment["voice#{i+1}title"])
           end
-        end
-      end
-    end #assert_nothing_raised
-  end #test_tp_make
+        end #project_default[:voice].each_with_index...
+      end #assignments.each d0....
+    ensure
+      tp_finish_outside_sandbox(dir, config_path)
+    end #begin
+    assert_all_assets_have_upload_status(assignments, ['audio'], 'no')
+  end
 
   def test_tp_make
     Dir.entries(audio_dir).select{|entry| File.directory?(File.join(audio_dir, entry))}.reject{|entry| entry.match(/^\./) }.each do |subdir|
@@ -73,7 +71,6 @@ class TestTpMake < Typingpool::Test::Script
         config_path = self.config_path(dir)
         skip_if_no_upload_credentials('tp-make integration test', Typingpool::Config.file(config_path))
         tp_make_with(dir, config_path, subdir)
-        tp_finish_outside_sandbox(dir, config_path)
       end #in_temp_tp_dir
     end #Dir.entries
   end
@@ -83,7 +80,6 @@ class TestTpMake < Typingpool::Test::Script
       skip_if_no_s3_credentials('tp-make S3 integration test', config)
       config_path = setup_s3_config(dir)
       tp_make_with(dir, config_path)
-      tp_finish_outside_sandbox(dir, config_path)
     end #in_temp_tp_dir do...
   end 
 
@@ -102,28 +98,26 @@ class TestTpMake < Typingpool::Test::Script
       assert(File.exists? File.join(project_dir, 'data', 'assignment.csv'))
       originals_dir = File.join(project_dir, 'audio', 'originals')
       refute_empty(Dir.entries(originals_dir).reject{|entry| entry.match(/^\./) }.map{|entry| File.join(originals_dir, entry) }.select{|path| File.file? path })
-      refute_empty(assignment_csv = File.read(File.join(project_dir, 'data', 'assignment.csv')))
-      refute_empty(assignment_rows = CSV.parse(assignment_csv))
-      assignment_headers = assignment_rows.shift
-      assert(assignment_uploaded_index = assignment_headers.find_index('audio_uploaded'))
-      assert_equal(assignment_rows.count, assignment_rows.select{|row| row[assignment_uploaded_index] == 'maybe' }.count)
-      assert(assignment_url_index = assignment_headers.find_index('audio_url'))
-      refute_empty(assignment_urls = assignment_rows.map{|row| row[assignment_url_index] })
-      assert_empty(assignment_urls.select{|url| working_url? url })
+      assert(project = temp_tp_dir_project(dir))
+      assert(assignment_csv = project.local.file('data', 'assignment.csv').as(:csv))
+      refute_empty(assignment_csv.to_a)
+      assert_all_assets_have_upload_status(assignment_csv, ['audio'], 'maybe')
+      assert(audio_urls = assignment_csv.map{|assignment| assignment['audio_url'] })
+      refute_empty(audio_urls)
+      assert_empty(audio_urls.select{|url| working_url? url })
       begin
         tp_make(dir, good_config_path, 'mp3')
-        refute_empty(assignment_csv = File.read(File.join(project_dir, 'data', 'assignment.csv')))
-        refute_empty(assignment_rows = CSV.parse(assignment_csv))
-        assignment_rows.shift
-        assert_equal(assignment_rows.count, assignment_rows.select{|row| row[assignment_uploaded_index] == 'yes' }.count)
-        refute_empty(assignment_urls2 = assignment_rows.map{|row| row[assignment_url_index] })
-        assignment_urls.each_with_index do |original_url, i|
-          assert_equal(original_url, assignment_urls2[i])
+        refute_empty(assignment_csv.read)
+        assert_all_assets_have_upload_status(assignment_csv, ['audio'], 'yes')
+        refute_empty(audio_urls2 = assignment_csv.map{|assignment| assignment['audio_url'] })
+        audio_urls.each_with_index do |original_url, i|
+          assert_equal(original_url, audio_urls2[i])
         end
-        assert_empty(assignment_urls2.reject{|url| working_url? url })
+        assert_equal(audio_urls.count, audio_urls2.select{|url| working_url? url }.count)
       ensure
         tp_finish_outside_sandbox(dir, good_config_path)
       end #begin
+      assert_all_assets_have_upload_status(assignment_csv, ['audio'], 'no')
     end #in_temp_tp_dir do...
   end
 
