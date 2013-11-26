60aa9685649667d0da7f65349e37042c2033e366
Ryan Tate <ryantate@ryantate.com>
2013-11-26 11:34:32 -0800

    Fixes reward display and pre-checking bug in bin/tp-assign.
    Previously, tp-assign failed to display the correct cost of assignment
    in the confirmation message when using the default reward (as read from
    the config file). When using the default reward, a total assignment
    cost of $0 was displayed. Also when using the default reward,
    tp-assign failed to properly calculate the total cost of the
    assignment run when pre-checking the cost of the assignments vs user's
    available balance.
    
    The correct reward was always sent to Amazon servers when actually
    assigning the task. This could hypothetically cause a batch of
    assignments to fail partway through if the default reward was being
    used and if the total account balance was exhausted before all
    assignments had been issued, since the pre-assignment total cost check
    had not been propertly computed.
    
    This commit fixes to-assign to display the same cost in the
    confirmation message it uses when assigning, which will correctly
    reflect the default reward when the default reward is being used. The
    correct cost is also used when pre-computing the total assignment
    cost.
    
    In a related change, tp-assign is also more conservative when rounding
    the final assignment run total cost, always rounding UP to the nearest
    cent. This prevents a hypothetical situation in which 1) Amazon rounds
    total cost of an assignment run UP (not confirmed, but not
    disconfirmed), 2) the total cost of an assignment run exceeds the
    user's balance by one penny, 3) the assignment run then fails when
    trying to assign the very last assignment. Also, tp-assign makes sure
    to always use the minimum Amazon commission charge when pre-computing
    the total, in the event someone tries to offer a reward of less than 5
    cents.
    
    Includes a regression test added to
    test_integration_script_3_tp_assign and related fixtures.
    
    This commit fixes #3. Thanks to @vitorio on github for the bug report.

--- test/test_integration_script_3_tp_assign.rb
+++ test/test_integration_script_3_tp_assign.rb
@@ -162,4 +162,32 @@ class TestTpAssign < Typingpool::Test::Script
     end #with_temp_readymade_project do...
   end
 
+  def test_displays_and_uses_correct_reward_default
+    with_temp_readymade_project do |dir| 
+      project = transcripts_dir_project(dir)
+      config = Typingpool::Config.file(config_path(dir))
+      config.assign.reward = '0.06'
+      write_config(config, File.dirname(config_path(dir)), File.basename(config_path(dir)))
+      vcr_names = ['tp_assign_6', 'tp_assign_7']
+      copy_tp_assign_fixtures(dir, vcr_names[0])
+      config = Typingpool::Config.file(config_path(dir))
+      assert_equal('0.06', config.assign.reward.to_s)
+      Typingpool::Amazon.setup(:sandbox => true, :config => config)
+      with_vcr(vcr_names[1], config, {
+                 :preserve_exact_body_bytes => true,
+                 :match_requests_on => [:method, Typingpool::App.vcr_core_host_matcher]
+               }) do
+        begin
+          out, err = tp_assign_with_vcr(dir, vcr_names[0])
+          assert_match(/would cost \$((0\.40)||(0\.47))\./, err)
+          refute_empty(results = Typingpool::Amazon::HIT.all_for_project(project.local.id))
+          assert_equal('0.06', results.first.at_amazon.reward_amount.to_s)
+      ensure
+        tp_finish(dir) if (Typingpool::Test.record || Typingpool::Test.live)
+      end #begin
+    end #with_temp_readymade_project do...
+  end #with_vcr...
+end
+
+
 end #TestTpAssign
